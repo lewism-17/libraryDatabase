1b38c49c5059432860ed25b79daf098aea8742dd
lewism-17 <q15lmorgan@wgsf.net>
2025-03-10 15:04:19 +0000

    Update books.py

--- books.py
+++ books.py
@@ -14,30 +14,42 @@ class bookFrame(tk.Frame):
 
         l2 = tk.Label(self, text = "Search by Title")
         l2.grid(row=1, column=0)
-        self.titlesearchbox = tk.Entry(self, width = 18)
-        self.titlesearchbox.grid(row=2, column = 0)
+        self.titlesearchbox = tk.Entry(self, width=18)
+        self.titlesearchbox.grid(row=2, column=0)
 
         l3 = tk.Label(self, text = "Search by Author")
         l3.grid(row=1, column=1)
-        self.authorsearchbox = tk.Entry(self, width = 18)
-        self.authorsearchbox.grid(row=2, column = 1)
+        self.authorsearchbox = tk.Entry(self, width=18)
+        self.authorsearchbox.grid(row=2, column=1)
 
         l4 = tk.Label(self, text = "Search by Year")
         l4.grid(row=4, column=0)
-        self.yearsearchbox = tk.Entry(self, width = 18)
-        self.yearsearchbox.grid(row=5, column = 0)
+        self.yearsearchbox = tk.Entry(self, width=18)
+        self.yearsearchbox.grid(row=5, column=0)
         
         l5 = tk.Label(self, text = "Search by Genre")
         l5.grid(row=4, column=1)
-        self.genresearchbox = tk.Entry(self, width = 18)
-        self.genresearchbox.grid(row=5, column = 1)
+        self.genresearchbox = tk.Entry(self, width=18)
+        self.genresearchbox.grid(row=5, column=1)
 
         l6 = tk.Label(self, text = "Available", font = self.titlefont)
-        l6.grid(row = 6, column = 0, columnspan = 2, stick = "EW")
+        l6.grid(row=6, column=0, columnspan=2, sticky = "EW")
 
         l7 = tk.Label(self, text = "Your Collection", font = self.titlefont)
-        l7.grid(row = 1, column = 2, columnspan = 2, stick = "EW")
+        l7.grid(row=1, column=2, columnspan=3, sticky = "EW")
+
+        self.booklist = tk.Listbox(self,width=30, height=20)
+        self.booklist.grid(row=7, column=0)
+
+        self.ownBooks = tk.Listbox(self,width=30)
+        self.ownBooks.grid(row=3, column=3, rowspan = 10)
 
     def loadUp(self):
         # put code in here to be run when this frame is displayed
+        results = self.parent.cursor.execute("SELECT * FROM books")
+        results = results.fetchall()
+        for i in results:
+            self.booklist.insert(tk.END, i[1])
+        
+        
         pass
